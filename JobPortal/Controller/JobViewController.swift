c3baf208b6597d5b7cfab4e023b5d68b832eefb3
muhammadmujtaba19 <k163863@nu.edu.pk>
2020-06-03 18:56:23 +0500

    Search bar functionality inside JobController

--- JobPortal/Controller/JobViewController.swift
+++ JobPortal/Controller/JobViewController.swift
@@ -8,16 +8,19 @@
 
 import UIKit
 import Alamofire
-class JobViewController: UIViewController {
+class JobViewController: UIViewController{
     
     @IBOutlet weak var JobTableView: UITableView!
     var id :Int?
-    @IBOutlet weak var tabBar: UITabBar!
+    @IBOutlet weak var searchBar: UISearchBar!
     var jobs = [Job]()
+    var filteredjobs = [Job]()
     override func viewDidLoad() {
         super.viewDidLoad()
+        searchBar.delegate = self
         JobTableView.delegate = self
         JobTableView.dataSource = self
+        
             AF.request("http://127.0.0.1:8080/api/Job/GetAllJobs").responseJSON { (response) in
                 do{
 //                    print(response)
@@ -28,6 +31,8 @@ class JobViewController: UIViewController {
                                 self.jobs.append(model)
                                 
                             }
+                    self.filteredjobs = self.jobs
+
                     self.JobTableView.reloadData()
                 }catch{
 
@@ -38,23 +43,37 @@ class JobViewController: UIViewController {
         
         JobTableView.rowHeight = 70
         
-
         // Do any additional setup after loading the view.
     }
     
+}
+extension JobViewController:UISearchBarDelegate{
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+           // When there is no text, filteredData is the same as the original data
+           // When user has entered text into the search box
+           // Use the filter method to iterate over all items in the data array
+           // For each item, return true if the item should be included and false if the
+           // item should NOT be included
+        filteredjobs = searchText.isEmpty ? jobs : jobs.filter { (item: Job) -> Bool in
+               // If dataItem matches the searchText, return true to include it
+            return item.Title?.range(of: searchText, options: .caseInsensitive, range: nil, locale: nil) != nil
+           }
+           
+           JobTableView.reloadData()
+       }
 }
 extension JobViewController:UITableViewDelegate{
     
 }
 extension JobViewController:UITableViewDataSource{
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return  jobs.count
+        return  filteredjobs.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "JobCell", for: indexPath) as! JobViewCell
-        cell.JobTitle?.text = jobs[indexPath.row].Designation
-        cell.JobCompany?.text = jobs[indexPath.row].Organization
+        cell.JobTitle?.text = filteredjobs[indexPath.row].Designation
+        cell.JobCompany?.text = filteredjobs[indexPath.row].Organization
         
         return cell
         
@@ -62,13 +81,11 @@ extension JobViewController:UITableViewDataSource{
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         id = indexPath.row
         self.performSegue(withIdentifier: "jobDetail", sender: self)
-    
-}
+        
+    }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if(segue.identifier=="jobDetail"){
-            // Create a variable that you want to send
-             let destinationVC = segue.destination as! JobDetailViewController
-            
+            print("here")
     }
     }
 }
